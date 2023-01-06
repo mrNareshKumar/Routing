5a80d6e1386e870f47416ac495aca558e9b0f4cd
root <naresh.sakinala@outlook.com>
2023-01-06 18:51:57 +0000

    routing

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about_container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about_image"
+    />
+    <h1 className="about_heading">About</h1>
+    <p className="about_paragraph">
+      I love to create! I am a frontend web developer
+    </p>
+  </div>
+)
+
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="notfound_container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not found"
+      className="notfound_image"
+    />
+    <h1 className="notfound_heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
